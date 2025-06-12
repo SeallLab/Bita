ffede9411cf37151ff83e8bee5ca0b5e9dacc701
keeryn04 <keerynjohnson@gmail.com>
2025-06-11 20:26:13 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = process.env.VITE_BACKEND_URL;
 
 function App() {
   const [sessionId, setSessionId] = useState("");
