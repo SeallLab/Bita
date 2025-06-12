4747ef6ae3d6b82d10a2068d7b251d28f58199ec
keeryn04 <keerynjohnson@gmail.com>
2025-06-11 18:42:43 -0600

    (Update) Changed Supabase Connection Creation

--- api/database_connector.py
+++ api/database_connector.py
@@ -1,17 +1,30 @@
 import os
 from dotenv import load_dotenv
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from supabase import create_client, Client
 
 #Load environment variables
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+class SupabaseSingleton:
+    _instance: Client = None
 
-def get_db_connection():
-    try:
-        conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
-        return conn
-    except psycopg2.Error as err:
-        print(f"Database connection error: {err}")
-        return None
+    @classmethod
+    def get_instance(cls) -> Client:
+        """Returns a singleton instance of the Supabase client."""
+        if cls._instance is None:
+            try:
+                supabase_url = os.getenv("SUPABASE_URI")
+                supabase_service_key = os.getenv("SUPABASE_SERVICE_KEY")
+
+                if not supabase_url or not supabase_service_key:
+                    raise ValueError("Supabase URL or Service Key is missing.")
+
+                cls._instance = create_client(supabase_url, supabase_service_key)
+            except Exception as e:
+                print(f"Supabase connection error: {e}")
+                cls._instance = None
+        return cls._instance
+
+def get_db_connection() -> Client:
+    """Returns the singleton instance of the Supabase client."""
+    return SupabaseSingleton.get_instance()
